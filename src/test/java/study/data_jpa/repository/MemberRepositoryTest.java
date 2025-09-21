ec412514d27c77340f5468789c86962e1924280f
APapeIsName <gms72901217@gmail.com>
2025-09-21 19:15:57 +0900

    21.@EntityGraph

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -257,4 +257,37 @@ class MemberRepositoryTest {
         //then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member1", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        // N+1 문제 - 1은 select Member, N은 Member 별 팀 정보를 전부 구해옴 -> 성능 문제 발생
+        // -> JPA 에서는 fetch join 으로 해결 가능
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member = " + member.getTeam().getName());
+        }
+    }
 }
